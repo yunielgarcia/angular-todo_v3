73abbdfa91655a337bd2ab9ce54e983ddbea4565
Yuniel Garcia <garciayuni84@gmail.com>
2018-07-09 12:12:51 -0400

    displaying todos

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('todoListApp', ['ngResource'])
-.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-}]);
+    .config(['$qProvider', function ($qProvider) {
+        $qProvider.errorOnUnhandledRejections(false);
+    }])
+    .config(function ($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    });
--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -1,23 +1,31 @@
 'use strict';
 
 angular.module('todoListApp')
-.controller('mainCtrl',['$scope', 'Todo', function($scope, Todo){
-  
-  $scope.todos = Todo.get().$promise
-        .then(function (data) {
-        // do something with the response
-            return data.todos;
-        }, function (error) {
-            // pass the error the the error service
-            console.log(error)
-            return []
-        });;
-  
-  $scope.addTodo = function() {
-    var todo = new Todo();
-    todo.name = 'New task!'
-    todo.completed = false;
-    $scope.todos.unshift(todo);
-  };
-  
-}])
+    .controller('mainCtrl', ['$scope', 'Todo', function ($scope, Todo) {
+
+        $scope.todos = [];
+
+
+        $scope.addTodo = function () {
+            var todo = new Todo();
+            todo.name = 'New task!'
+            todo.completed = false;
+            // $scope.todos.unshift(todo);
+            console.log('angular working')
+        };
+
+        function fetchAll() {
+            Todo.get().$promise
+                .then(function (data) {
+                    // do something with the response
+                    $scope.todos = data.todos;
+                }, function (error) {
+                    // pass the error the the error service
+                    console.log(error)
+                    return []
+                });
+        }
+
+        //Execute
+        fetchAll();
+    }]);
