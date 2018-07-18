c45b456370fcf73518004bcb82518f0dd8362a1d
Yuniel Garcia <garciayuni84@gmail.com>
2018-07-18 10:09:53 -0400

    token in place. Hard coded for angular.

--- static/scripts/resources/todo.js
+++ static/scripts/resources/todo.js
@@ -2,12 +2,38 @@
 
 angular.module('todoListApp')
     .factory('Todo', function ($resource) {
+        var token = 'eyJpYXQiOjE1MzE5MjA3NDgsImV4cCI6MTUzMTkyNDM0OCwiYWxnIjoiSFMyNTYifQ.eyJpZCI6MX0.C4_L8efLiK7TQc8HxUAhwc1FJqz66M535h_-7HIMDN8'
         return $resource('/api/v1/todos/:id', {id: '@id'}, {
             update: {
-                method: 'PUT'
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Token ' + token
+                }
             },
             get: {
-                method: 'GET', isArray: false
-            }
+                method: 'GET',
+                isArray: false,
+                headers: {
+                    'Authorization': 'Token ' + token
+                }
+            },
+            delete: {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Token ' + token
+                }
+            },
         });
+    })
+    .factory('TokenHandler', function () {
+        var tokenHandler = {};
+        var token = "none";
+
+        tokenHandler.set = function (newToken) {
+            token = newToken;
+        };
+
+        tokenHandler.get = function () {
+            return token;
+        }
     });
